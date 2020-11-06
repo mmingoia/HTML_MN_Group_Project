f9e9b83599ee633649458c2f6052304a4883724f
Tim Smith <tgs584@utexas.edu>
2020-11-06 12:26:38 -0600

    Updated routes

--- Dashboard_Structure/Dashboardapp.py
+++ Dashboard_Structure/Dashboardapp.py
@@ -9,7 +9,7 @@ from flask import Flask, jsonify
 from flask import Flask, render_template
 
 
-### < NEED TO FIGURE OUT CONNECTING TO OUR DATABASE HERE >
+# ***** NEED TO FIGURE OUT CONNECTING TO OUR DATABASE HERE ******
 
 #engine = create_engine("sqlite:///hawaii.sqlite")
 
@@ -27,10 +27,32 @@ app = Flask(__name__)
 #----------------------
 # Creating the routes
 
-## < NEED TO EDIT THE ROUTES TO REFLECT THE DIFFERENT PAGES W WILL HAVE > 
+## < NEED TO EDIT THE ROUTES TO REFLECT THE DIFFERENT PAGES WE WILL HAVE > 
 @app.route("/")
 def homepage():
         return render_template ("welcomepage.html") 
+
+
+
+@app.route("/Turnout_Demographics")
+#-------Route will return the webpage with turnout based on voter demographics
+
+
+@app.route("/NonVoter_Reasons")
+
+#----- Route will return the webpage with reasons why people don't turn out
+
+@app.route("")
+
+
+
+@app.route("/Predictive_Model")
+#----- FIGURE OUT CONNECTING TO OUR MACHINE LEARNING MODEL HERE
+
+
+
+
+
 
 
 if __name__ == "__main__":
