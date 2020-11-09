508b15a9c8481401de02019cf82b0366e9fe7cc5
Hannah <66184980+HannahEG@users.noreply.github.com>
2020-11-09 02:28:15 -0700

    formatting tweaks

--- app/Dashboardappcopy.py
+++ app/Dashboardappcopy.py
@@ -19,13 +19,13 @@ from pickle import load
 # ***** CONNECTING TO OUR DATABASE ******
 
 
-t_host = "provisionaldb2.cpvxmi357s0k.us-east-2.rds.amazonaws.com" # either "localhost", a domain name, or an IP address.
-t_port = "5432" # default postgres port
-t_dbname = "GroupProjectDB"
-t_user = "postgres"
-t_pw = "postgres"
-db_conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_dbname, user=t_user, password=t_pw)
-db_cursor = db_conn.cursor()
+# t_host = "provisionaldb2.cpvxmi357s0k.us-east-2.rds.amazonaws.com" # either "localhost", a domain name, or an IP address.
+# t_port = "5432" # default postgres port
+# t_dbname = "GroupProjectDB"
+# t_user = "postgres"
+# t_pw = "postgres"
+# db_conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_dbname, user=t_user, password=t_pw)
+# db_cursor = db_conn.cursor()
 
 
 
@@ -76,17 +76,7 @@ def visualize2():
     return render_template("Competitiveness.html")
 
 
-
-# #****** FIGURE OUT CONNECTING TO OUR MACHINE LEARNING MODEL HERE**********
-# def prediction():
-
-# @app.route('/PredictTurnout')
-# def blankform():
-#     return render_template('model.html')
-
-input_cols = ['reg_vep', 'midterm', 'black', 'white', 'nonvoter']
-
-# # prediction function 
+# Function to utilize saved RandomForestModel
 def ValuePredictor(user_input): 
     y_predict = np.array(user_input).reshape(1,5)
     loaded_model = joblib.load("rf_model.pkl")
@@ -98,34 +88,14 @@ def ValuePredictor(user_input):
 def result():  
     if request.method == 'POST': 
         user_input = request.form.to_dict() 
-        # print(user_input)
-        # user_input = user_input.values()
         user_input = list(user_input.values()) 
         user_input = list(map(int, user_input)) 
         result = ValuePredictor(user_input)         
         if result > 0: 
-            prediction = round(result * 100)
-        # else: 
-        #     prediction ='Income less that 50K'            
+            prediction = round(result * 100, 2)            
         return render_template("model.html", prediction = prediction) 
     return render_template('model.html')
-
  
-    # if request.method == 'POST': 
-    #     submit = request.form['button']
-    #     # registered_voters = request.form['reg_vep']
-    
-    #     user_input = request.form.to_dict() 
-    #     # to_predict_list = pd.DataFrame(to_predict_list)
-    #     # to_predict_list = list(to_predict_list.values()) 
-    #     # to_predict_list = list(map(int, to_predict_list)) 
-    #     result = ValuePredictor(Form)         
-    #     if int(result) > 0: 
-    #         prediction = '{result * 100 } % percent likely to vote'            
-    #     return render_template("model.html", prediction = prediction)     
 
 if __name__ == "__main__":
     app.run()
-
-# %%
- 
