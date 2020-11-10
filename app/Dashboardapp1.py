e10f0f8fb9ae0ca298aaf9d1de4b88d73ee201be
Hannah <66184980+HannahEG@users.noreply.github.com>
2020-11-09 21:20:05 -0700

    added flask dependency

--- app/Dashboardapp1.py
+++ app/Dashboardapp1.py
@@ -2,7 +2,7 @@
 #Import Dependencies
 import numpy as np
 import pandas as pd
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import joblib
 
 #**********************************************
