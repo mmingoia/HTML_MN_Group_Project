886822fb4701dfabdb578e8039bf4ed909e8ec1b
Hannah <66184980+HannahEG@users.noreply.github.com>
2020-11-14 12:28:28 -0700

    TLC

--- app/static/form.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var reg_vepItem = sessionStorage.getItem("2reg_vep");  
-    ('2reg_vep').val(reg_vepItem);
-    }
-    ('2reg_vep').change(function() { 
-        var reg_vepVal = (this).val();
-        sessionStorage.setItem("2reg_vep", reg_vepVal);
-    });
